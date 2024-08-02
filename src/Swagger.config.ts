ee69ed99e026e9f080c89526a87ab51f6f0d89e9
Porcelanic <porcelanic0907@gmail.com>
2024-08-02 06:36:59 -0500

    Preparing for merge
    The state of the 'main' branch is less than optimal for a merge, so almost all of the files in it have been deleted in favor of just merging with the a more functional branch

--- src/Swagger.config.ts
+++
@@ -1,7 +0,0 @@
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-
-export const setupSwagger = (app) => {
-  const config = new DocumentBuilder().build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api-docs', app, document);
-};
